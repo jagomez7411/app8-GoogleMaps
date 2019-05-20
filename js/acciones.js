fedc19bc00236016fcef8902943c7e8555352516
Antonio Gomez <aredcbtis16@hotmail.com>
2019-05-19 20:19:10 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -11,13 +11,12 @@ $('#posicion').on ('click', function (){
 	getPosition();
 	});
 	
-$('#watch').on('click', function (){
-	showPosition();
-});	
+//$('#watch').on('click', function (){
+	//showPosition();
+//});	
 }
 
-	function getPosition() {
-
+function getPosition() {
    var options = {
       enableHighAccuracy: true,
       maximumAge: 3600000
@@ -25,26 +24,26 @@ $('#watch').on('click', function (){
 	
    var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
-   function onSuccess(position) {
+function onSuccess(position) {
 
       alert('Latitude: '       + position.coords.latitude          + '\n' +
          'Longitude: '         + position.coords.longitude         + '\n' +
          'Altitude: '          + position.coords.altitude          + '\n');
    };
 
-   function onError(error) {
+function onError(error) {
       alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
    }
 }
 
-function showPosition(position){
-  latitud=position.coords.latitude;
-  longitud=position.coords.longitude;
-  d.innerHTML+="<p>Latitud: "+latitud+"</p>";
-  d.innerHTML+="<p>Longitud: "+longitud+"</p>";
-  latlon=latitud+","+longitud;
-  var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
-     +latlon+"&zoom=14&size=350x250&sensor=false";
-  d.innerHTML+="<img src='"+img_url+"'>";
-}
+//function showPosition(position){
+//  latitud=position.coords.latitude;
+//  longitud=position.coords.longitude;
+//  d.innerHTML+="<p>Latitud: "+latitud+"</p>";
+//  d.innerHTML+="<p>Longitud: "+longitud+"</p>";
+//  latlon=latitud+","+longitud;
+//  var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
+//     +latlon+"&zoom=14&size=350x250&sensor=false";
+//  d.innerHTML+="<img src='"+img_url+"'>";
+//}
 
